b12ca7f1e2bbf5d267e43ead8ec9b525350e0b2e
borkarfaiz <borkarfaiz@gmail.com>
2016-12-26 14:46:42 +0530

    created some basic views

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: polls/
     url(r'^$', views.index, name='index'),
+
+    # ex: polls/432
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # ex: polls/432/result
+    url(r'^(?P<question_id>[0-9]+)/result/$', views.results, name='result'),
+
+    # ex: polls/463/vote
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("You're at the homepage")
+    latest_questoin_list = Question.objects.order_by('pub_date')[:5:-1]
+    output = ', '.join([q.question_text for q in latest_questoin_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    response = "You're looking at question {}".format(question_id)
+    return HttpResponse(response)
+
+
+def results(request, question_id):
+    result = "You're looking at the results of question {}".format(question_id)
+    return HttpResponse(result)
+
+
+def vote(request, question_id):
+    return HttpResponse("Yo're voting on question {}".format(question_id))
 
