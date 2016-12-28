afe8264cb3722797d270ca5902c79a1bccd7228a
borkarfaiz <borkarfaiz@gmail.com>
2016-12-28 12:39:33 +0530

    previous commit work done here

--- polls/urls.py
+++ polls/urls.py
@@ -7,13 +7,10 @@ app_name = 'polls'
 urlpatterns = [
     # ex: polls/
     url(r'^$', views.index, name='index'),
-
     # ex: polls/432
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-
     # ex: polls/432/result
-    url(r'^(?P<question_id>[0-9]+)/result/$', views.results, name='result'),
-
+    url(r'^(?P<question_id>[0-9]+)/result/$', views.results, name='results'),
     # ex: polls/463/vote
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
