c6148be310500d76e949e67f7cf1dc9c72e14c98
borkarfaiz <borkarfaiz@gmail.com>
2016-12-29 13:59:09 +0530

    tutorial 4 completed basic app has been created

--- polls/urls.py
+++ polls/urls.py
@@ -6,11 +6,11 @@ app_name = 'polls'
 
 urlpatterns = [
     # ex: polls/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex: polls/432
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: polls/432/result
-    url(r'^(?P<question_id>[0-9]+)/result/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/result/$', views.ResultsView.as_view(), name='results'),
     # ex: polls/463/vote
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
