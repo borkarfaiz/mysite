42a59be93315f73194afad20385971550b72b2d5
borkarfaiz <borkarfaiz@gmail.com>
2016-12-30 15:38:51 +0530

    added style sheets tutorial 6 completed

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('homepage.urls')),
+    url(r'^', include('homepage.urls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
